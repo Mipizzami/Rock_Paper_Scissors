be62ef371da491c23181c10b2fdd73016323df8c
Mipizzami <167435216+Mipizzami@users.noreply.github.com>
2024-10-29 16:34:28 +0100

    Do the game without a counter

--- javascript.js
+++ javascript.js
@@ -4,13 +4,9 @@ function getComputerChoice(max){
     return Math.floor(Math.random() * max)
 }
 
-function getHumanChoice(x){
-    
-}
-
+function getHumanChoice(){
 let choice = prompt("What's your choice? Rock/Paper/Scissors");
 let x = 0;
-
 if (choice.toLowerCase() == "rock"){
     alert('you chose rock');
 }
@@ -25,12 +21,13 @@ else if (choice.toLowerCase() == "scissors"){
 else {
     alert('you spelled your choice wrong');
 }
+return x;
+}
 
 let HumanScore = 0;
-let ComputerScore = 0;
+let ComputerScore = 0;  
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
+function playRound(humanChoice, computerChoice, HumanScore, ComputerScore, HumanScore, ComputerScore){
     // Both of you took the same
     if (humanChoice == computerChoice){
         alert('DRAW!');
@@ -39,29 +36,47 @@ function playRound(humanChoice, computerChoice){
     if (humanChoice == 0){
         if (computerChoice == 1){
             alert('You Lose, Computer chose Paper');
+            ComputerScore++;
         }
         else if (computerChoice == 2){
             alert('You win, Computer chose Scissors');
+            HumanScore++;
         }
     }
     // You chose paper
-    if (humanChoice == 1){
+    else if (humanChoice == 1){
         if (computerChoice == 2){
             alert('You Lose, Computer chose Scissors');
+            ComputerScore++;
         }
         else if (computerChoice == 0){
             alert('You win, Computer chose Rock');
+            HumanScore++;
         }
     }
 
-    if (humanChoice == 2){
+    else if (humanChoice == 2){
         if (computerChoice == 0){
             alert('You Lose, Computer chose Rock');
+            ComputerScore++;
         }
         else if (computerChoice == 1){
             alert('You win, Computer chose Paper');
+            HumanScore++;
         }
     }
 
 }
 
+function playGame(){
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice(3);
+
+    playRound(humanChoice, computerChoice, HumanScore, ComputerScore, HumanScore, ComputerScore);
+}
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
