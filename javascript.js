cc6a69992a786e73aa2463ad18e6235ece8031d2
Mipizzami <167435216+Mipizzami@users.noreply.github.com>
2024-10-29 15:11:52 +0100

    Add Playround function

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function getComputerChoice(max){
 }
 
 function getHumanChoice(x){
-
+    
 }
 
 let choice = prompt("What's your choice? Rock/Paper/Scissors");
@@ -30,5 +30,38 @@ let HumanScore = 0;
 let ComputerScore = 0;
 
 function playRound(humanChoice, computerChoice){
-    
+    humanChoice = humanChoice.toLowerCase();
+    // Both of you took the same
+    if (humanChoice == computerChoice){
+        alert('DRAW!');
+    }
+    // You chose rock
+    if (humanChoice == 0){
+        if (computerChoice == 1){
+            alert('You Lose, Computer chose Paper');
+        }
+        else if (computerChoice == 2){
+            alert('You win, Computer chose Scissors');
+        }
+    }
+    // You chose paper
+    if (humanChoice == 1){
+        if (computerChoice == 2){
+            alert('You Lose, Computer chose Scissors');
+        }
+        else if (computerChoice == 0){
+            alert('You win, Computer chose Rock');
+        }
+    }
+
+    if (humanChoice == 2){
+        if (computerChoice == 0){
+            alert('You Lose, Computer chose Rock');
+        }
+        else if (computerChoice == 1){
+            alert('You win, Computer chose Paper');
+        }
+    }
+
 }
+
